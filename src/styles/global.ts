b78ab19eb14eff2cbc77f1915f09d406fd8180e9
Felipe Machado <felipemmachado28@gmail.com>
2021-05-11 14:06:16 -0300

    subindo os fontes

---
+++ src/styles/global.ts
@@ -0,0 +1,34 @@
+import { createGlobalStyle } from 'styled-components';
+
+import githubLogo  from '../assets/github-icon.png';
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background: #F0F0F5 url(${githubLogo}) no-repeat 70% top;
+    background-size: 500px;
+    -webkit-font-smoothing: antialiased;
+    filter: opacity(90)
+
+  }
+
+  body, input, button {
+    font: 16px Roboto, sans-serif;
+  }
+
+  #root {
+    max-width:960px;
+    margin: 0 auto;
+    padding: 40px 20px;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`
